b52a1bad223cdc9869583862862bbe395efb1529
skip mcbride <skip@angerbunny.com>
2025-11-18 07:22:32 -0800

    stashing changes

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	typedRoutes: true,
 	reactCompiler: true,
+
+  allowedDevOrigins: ['192.168.68.71', '*.local-origin.dev'], // future proofing for dev environments
 };
 
 export default nextConfig;
