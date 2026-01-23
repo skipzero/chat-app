dbf745e821e3a1437519b5e804c15711013cee0d
skip mcbride <skip@angerbunny.com>
2026-01-22 16:49:11 -0800

    adding rooms routing and page

---
+++ packages/api/src/routers/rooms.ts
@@ -0,0 +1,29 @@
+import { Room } from "@chat/db/models/room.model";
+import z from "zod";
+
+import { protectedProcedure } from "../index";
+
+export const roomsRouter = {
+  getAll: protectedProcedure.handler(async () => {
+    return await Room.find().lean();
+  }),
+
+  create: protectedProcedure
+    .input(z.object({ text: z.string().min(1) }))
+    .handler(async ({ input }) => {
+      const newRoom = await Room.create({ text: input.text });
+      return newRoom.toObject();
+    }),
+
+  // toggle: publicProcedure
+  //   .input(z.object({ id: z.string(), completed: z.boolean() }))
+  //   .handler(async ({ input }) => {
+  //     await Room.updateOne({ id: input.id }, { completed: input.completed });
+  //     return { success: true };
+  //   }),
+
+  delete: protectedProcedure.input(z.object({ id: z.number() })).handler(async ({ input }) => {
+    await Room.deleteOne({ id: input.id });
+    return { success: true };
+  }),
+};
