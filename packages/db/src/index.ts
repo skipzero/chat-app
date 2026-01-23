3c9d7e1947542cf7278104205f5f60c9f5ebf83f
skip mcbride <skip@angerbunny.com>
2026-01-22 16:14:33 -0800

    fixed db stuff

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -5,6 +5,6 @@ await mongoose.connect(env.DATABASE_URL).catch((error) => {
   console.log("Error connecting to database:", error);
 });
 
-const client = mongoose.connection.getClient().db("myDB");
+const client = mongoose.connection.getClient().db(env.DATABASE_NAME);
 
 export { client };
