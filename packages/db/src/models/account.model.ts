5eb99c13a84f29f59becfbc6cd95d5061fd2f67b
skip mcbride <skip@angerbunny.com>
2026-01-25 02:35:35 -0800

    adding new models

---
+++ packages/db/src/models/account.model.ts
@@ -0,0 +1,18 @@
+import { model, Schema } from "mongoose";
+
+const accountSchema = new Schema(
+	{
+		_id: { type: String },
+		accountId: { type: String, required: true },
+		providerId: { type: String, required: true },
+		userId: { type: String, ref: "User", required: true },
+		idToken: { type: String },
+		role: { type: String },
+		password: { type: String },
+		createdAt: { type: Date, required: true },
+		updatedAt: { type: Date, required: true },
+	},
+	{ collection: "account" },
+);
+
+export const Account = model("Account", accountSchema);
