62ddee27fe82a330fdcabcbcb616724339b9a2f8
skip mcbride <skip@angerbunny.com>
2026-03-22 09:05:22 -0700

    removed orpc api modules

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,7 +1,6 @@
 import { env } from "@chatapp/env/server";
 import mongoose from "mongoose";
 
-
 const { DATABASE_NAME, DATABASE_URL } = env;
 await mongoose.connect(DATABASE_URL).catch((error) => {
   console.log("Error connecting to database:", error);
