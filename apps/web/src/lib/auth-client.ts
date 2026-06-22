51f291e58426739d812ced6be150be44d915cc8f
skip mcbride <skip@angerbunny.com>
2026-06-22 13:46:08 -0700

    explicitly set url to override default

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,4 +1,12 @@
 import { createAuthClient } from "better-auth/react";
 
-export const authClient = createAuthClient()
+export const authClient = createAuthClient({
+  baseURL: { 
+    allowedHosts: [
+      '*zerosquadron.com',
+      '*angerbunny.com',
+    ],
+    fallback: 'https://zerosquadron.com',
+    protocol: process.env.NODE_ENV === "development" ? "http" : "https",
+})
 export const { signIn, signUp, signOut, useSession, getSession } = authClient;
